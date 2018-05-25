b008f3bec97337875020835a0b89ee991abfc39d
Charlie Kobayashi <sparkychildcharlie@gmail.com>
2018-05-25 14:39:04 -0400

    Create formulas.js

---
+++ js/formulas.js
@@ -0,0 +1,90 @@
+var Formulas = [
+  {
+    weap_arm: 'w',
+    stats: [
+      ['ATK %', 7],
+      ['Critical Rate', 15],
+      ['Critical Rate %', 15],
+      ['Critical Damage', 15],
+      ['Natural HP Regen %', -7],
+      ['Natural MP Regen', -7],
+    ],
+  },
+  {
+    weap_arm: 'w',
+    stats: [
+      ['MATK %', 7],
+      ['INT %', 7],
+      ['INT', 14],
+      ['Dodge', -7],
+      ['Natural HP Regen %', -7],
+      ['MDEF %', -7],
+    ],
+  },
+  {
+    weap_arm: 'w',
+    stats: [
+      ['ATK %', 7],
+      ['Critical Damage %', 7],
+      ['Critical Damage', 15],
+      ['Dodge', -7],
+      ['Natural HP Regen %', -7],
+      ['MDEF %', -7],
+    ],
+  },
+  {
+    weap_arm: 'w',
+    stats: [
+      ['ATK %', 7],
+      ['Critical Damage %', 7],
+      ['STR %', 7],
+      ['Dodge', -7],
+      ['Natural HP Regen %', -7],
+      ['MDEF %', -7],
+    ],
+  },
+  {
+    weap_arm: 'a',
+    stats: [
+      ['Critical Damage %', 7],
+      ['Critical Rate', 15],
+      ['Critical Rate %', 15],
+      ['Critical Damage', 15],
+      ['MATK %', -7],
+      ['Magic Pierce %', -3],
+    ],
+  },
+  {
+    weap_arm: 'a',
+    stats: [
+      ['MATK %', 7],
+      ['INT %', 7],
+      ['Accuracy', -7],
+      ['Accuracy %', -3],
+      ['ATK %', -7],
+      ['Physical Pierce %', -3],
+    ],
+  },
+  {
+    weap_arm: 'a',
+    stats: [
+      ['MATK %', 7],
+      ['INT %', 7],
+      ['Aggro %', -10, 1],
+      ['Accuracy', -7],
+      ['ATK %', -7],
+      ['Physical Pierce %', -3],
+    ],
+  },
+  {
+    weap_arm: 'a',
+    stats: [
+      ['STR %', 7],
+      ['Critical Damage %', 7],
+      ['Critical Damage', 15],
+      ['MATK %', -7],
+      ['Magic Pierce %', -3],
+      ['Accuracy', -7],
+    ],
+  },
+];
