6f4bfe8fa6d31db5f602d0d1e3071d41494d4de6
Charlie Kobayashi <sparkychildcharlie@gmail.com>
2018-05-25 14:39:14 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -140,7 +140,7 @@ class SIMULATOR {
 
         let pot = 1;
         this.results = {}; // clean results!
-        while (true) {
+        while (pot < 1000) {
             let formula = new FORMULA(this, pot, this.recipe_pot, this.weap_arm, stats);
             formula.run();
 
@@ -148,7 +148,6 @@ class SIMULATOR {
                 if (formula.success === 100) pot = pot + '+';
                 this.results[pot] = formula;
             }
-
             if (formula.success === 100) break;
             pot++;
         }
@@ -218,10 +217,10 @@ class FORMULA {
         data.value = value;
         this.stats[slot] = data;
 
-        return { cost, potential, mat: stat.mat, step: this.toStepString(stat.name, value) };
+        return { cost, potential, mat: stat.mat, step: FORMULA.toStepString(stat.name, value) };
     }
 
-    toStepString(stat, value) {
+    static toStepString(stat, value) {
       let increment_indicator = value > 0 ? '+' : '-';
       value = Math.abs(value);
       if (stat.indexOf('%') === 0) return increment_indicator + value + stat;
@@ -256,9 +255,10 @@ class FORMULA {
         if (!potUsed) return 0;
 
         // apply mat and potential costs
-        for (let m in matUsed) this.mats[m] += matUsed[m];
+        for (let m in matUsed) this.mats[m] += Math.floor(matUsed[m]);
         potUsed = this.potFloor(potUsed);
         this.potential += potUsed;
+
 
         stepData = stepData.join(', ');
         this.steps.push([stepData, this.potential]);
@@ -274,146 +274,156 @@ class FORMULA {
         return [data.id, data.data, data.value];
     }
 
+    absolutePot(stat) {
+      let abs_pot = Math.abs(stat.data.pot * stat.value * (stat.data.type !== this.weap_arm && ['a', 'w'].includes(stat.data.type) ? 2 : 1));
+      return abs_pot;
+    }
+
     run() {
-            /**
-             * STEPS:
-             *
-             * 1. If there are any "repeated" stats that result in penalty, stat as much of the
-             * positives as possible - first the positives that result in penalties; if there are none,
-             * stick them all on one stat. Stat the biggest potential user first (Int+5%) then put the
-             * smaller pot ones in one step at a time until you approach 0 pot left.
-             *
-             * 2. Stat all the negs in one go - stat the bigger pot giving neg (MAtk-7% as opposed to M. Pierce-3%)
-             * first if the other one has to be left out later to leave one slot open (eg. statting fc15)
-             *
-             * 3. If there is any way of avoiding penalty points (steps that cost only 1 potential,
-             * such as crit rate, and cast speed) put them in one point at a time.
-             *
-             * 4. Fill in the rest of the stats all in one go.
-             */
+        /**
+         * STEPS:
+         *
+         * 1. If there are any "repeated" stats that result in penalty, stat as much of the
+         * positives as possible - first the positives that result in penalties; if there are none,
+         * stick them all on one stat. Stat the biggest potential user first (Int+5%) then put the
+         * smaller pot ones in one step at a time until you approach 0 pot left.
+         *
+         * 2. Stat all the negs in one go - stat the bigger pot giving neg (MAtk-7% as opposed to M. Pierce-3%)
+         * first if the other one has to be left out later to leave one slot open (eg. statting fc15)
+         *
+         * 3. If there is any way of avoiding penalty points (steps that cost only 1 potential,
+         * such as crit rate, and cast speed) put them in one point at a time.
+         *
+         * 4. Fill in the rest of the stats all in one go.
+         */
 
-            // determine any repeats first
-            let categories = [];
-            let repeated_categories = [];
+        // determine any repeats first
+        let categories = [];
+        let repeated_categories = [];
 
-            for (let s of this.final_stats) {
-                let cat = s.data.cat;
+        for (let s of this.final_stats) {
+            let cat = s.data.cat;
 
-                if (categories.includes(cat)) repeated_categories.push(cat);
-                else categories.push(cat);
-            }
-
-            let repeated_stats = this.final_stats.filter(s => s && repeated_categories.includes(s.data.cat));
-
-            // determine if there are any repeated stats that are positive;
-            let positive_repeats = repeated_stats.filter(s => s.value > 0);
-            let negs = this.final_stats.filter(s => s.value < 0);
-
-            if (positive_repeats.length && negs.length) {
-                // sort decending order of potential requirement
-                positive_repeats = positive_repeats.sort((a, b) => b.data.pot * (b.data.cat === this.weap_arm ? 2 : 1) - a.data.pot * (a.data.cat === this.weap_arm ? 2 : 1));
-                let remain_pot = 0;
-                let mock_stats = [];
-                // determine how much potential to leave behind - use a special type of reduce that applies penalty
-                for (let i = 0; i < positive_repeats.length; i++) {
-                    let r = positive_repeats[i];
-                    if (i === positive_repeats.length - 1) {
-                        mock_stats.push(r);
-                        continue; // there is no need to leave behind any penalty for the biggets and smallest pot stat. -> those can be added along with the negs.
-                    }
-                    mock_stats.push(r);
-                    let pen = this.determinePenalty(mock_stats);
-                    remain_pot += Math.round(pen * r.data.pot); // leave enough pot for one point of that stat.
-                }
-
-                // apply all the positive ones
-                let curr_pr_stat_id = 0;
-                while (true) {
-                    // determine number steps to take
-                    let s = positive_repeats[curr_pr_stat_id];
-
-                    let pen = this.determinePenalty([...this.stats, s]);
-                    let turn_remain_pot = remain_pot + 0;
-                    remain_pot -= pen * s.data.pot;
-                    if (remain_pot < 0) remain_pot = 0;
-
-                    let leave_behind = (!positive_repeats[curr_pr_stat_id + 2] && positive_repeats[curr_pr_stat_id + 1] && positive_repeats[curr_pr_stat_id + 1].data.pot === 1 ? 1 : 0);
-
-                    let available = (this.potential - remain_pot - leave_behind - 1) || 1; // always leave one remaining!
-                    let steps = Math.floor(available / s.data.pot);
-                    if (steps > s.value) steps = s.value;
-
-                    if (!curr_pr_stat_id) {
-                        // stat em all if they're the first one
-                         this.applyStat([s.id, s.data, steps, true]);
-                    } else {
-                        let used;
-                        // do it step by step
-                        for (let i = 0; i < steps; i++) {
-                            used = this.applyStat([s.id, s.data, 1, true]);
-                        }
-                    }
-
-                    let calc_pot = remain_pot <= 0 ? turn_remain_pot : remain_pot;
-                    if (this.potential <= calc_pot || curr_pr_stat_id + 1 === positive_repeats.length) break; // this means that it's done
-
-                    curr_pr_stat_id++;
-                }
-
-                let slotsLeft = this.stats.filter(s => !s).length; // empty slots
-
-                if (curr_pr_stat_id < positive_repeats.length - 1) {
-                    // there are still positives left - add the negs now along with the last positive
-                    curr_pr_stat_id++;
-                    let s = positive_repeats[curr_pr_stat_id];
-
-                    if (slotsLeft - 1 === negs.length) {
-                        // leave one behind!
-                        negs = negs.sort((a,b) => {
-                           let apen = this.determinePenalty([...this.stats, a]);
-                           let bpen = this.determinePenalty([...this.stats, b]);
-
-                           return bpen - apen || b.data.pot * (b.data.cat === this.weap_arm ? 2 : 1) * -b.value - a.data.pot * (a.data.cat === this.weap_arm ? 2 : 1) * -a.value;
-                        });
-
-                        let shouldStat = negs.slice(0, negs.length - 1);
-                        this.applyStat([s.id, s.data, 1, true], ...shouldStat.map(this.statToArray));
-                    } else {
-                        this.applyStat([s.id, s.data, 1, true], ...negs.map(this.statToArray));
-                    }
-                } else {
-                    if (slotsLeft === negs.length) {
-                        // leave one behind!
-                        negs = negs.sort((a,b) => {
-                           let apen = this.determinePenalty([...this.stats, a]);
-                           let bpen = this.determinePenalty([...this.stats, b]);
-
-                           return bpen - apen || b.data.pot * (b.data.cat === this.weap_arm ? 2 : 1) * -b.value - a.data.pot * (a.data.cat === this.weap_arm ? 2 : 1) * -a.value;
-                        });
-
-                        let shouldStat = negs.slice(0, negs.length - 1);
-                        this.applyStat(...shouldStat.map(this.statToArray));
-                    } else {
-                        this.applyStat(...negs.map(this.statToArray));
-                    }
-                }
-            } else {
-                this.applyStat(...negs.map(this.statToArray));
-            }
-
-            // now identify whichever ones are one pot - these are put in one at a time to avoid any penalties
-            let onepot = this.final_stats.filter(s => s.data.pot === 1);
-            for (let s of onepot) {
-                while ((this.stats[s.id] ? this.stats[s.id].value : 0) < s.value) {
-                    this.applyStat([s.id, s.data, 1, true]);
-                }
-            }
-
-            // finally fill everything
-            this.applyStat(...this.final_stats.map(this.statToArray));
-
-            this.determineSuccess();
+            if (categories.includes(cat)) repeated_categories.push(cat);
+            else categories.push(cat);
         }
+
+        let repeated_stats = this.final_stats.filter(s => s && repeated_categories.includes(s.data.cat));
+
+        // determine if there are any repeated stats that are positive;
+        let positive_repeats = repeated_stats.filter(s => s.value > 0).sort((a, b) => this.absolutePot(b) - this.absolutePot(a));
+        let negs = this.final_stats.filter(s => s.value < 0).sort((a, b) => this.absolutePot(b) - this.absolutePot(a));
+
+        let non_repeat_positive = this.final_stats.filter(s => s.value > 0 && !positive_repeats.some(st => s.name === st.name)).sort((a, b) => this.absolutePot(b) - this.absolutePot(a));
+        let stat_positives = [];
+
+        if (negs.length && repeated_stats.length) {
+          // there will be penalties later on, so use up the potential first
+          let pos_potential = 0;
+          let mock_stats = [];
+          for (let s of positive_repeats) {
+            mock_stats.push(s);
+            let pen = this.determinePenalty(mock_stats);
+            if (s.data.pot === 1) pos_potential += 1;
+            else pos_potential += s.data.pot * s.value * pen;
+          }
+          if (pos_potential < this.potential && non_repeat_positive.length) {
+            // since it wont use up all the potential before statting negatives, we will bring
+            // in some non-repeated positives so they can escape penalty after the negatives.
+            while (pos_potential < this.potential && non_repeat_positive.length) {
+              let new_add = non_repeat_positive.shift(); // take the first one in queue.
+              if (new_add.data.pot === 1) continue; // this doesn't matter - it'll still be one pot per step after.
+              pos_potential += this.absolutePot(new_add);
+              stat_positives.push(new_add);
+            }
+            // the non-repeated ones are always put in before the repeats so they do not incur penalty.
+            stat_positives = [...stat_positives, ...positive_repeats];
+          } else {
+            stat_positives = positive_repeats;
+          }
+
+          // start the statting
+          mock_stats = [];
+          let remain_pot = 0;
+          for (let s of stat_positives) {
+            mock_stats.push(s);
+            let pen = this.determinePenalty(mock_stats);
+            if (s.data.pot === 1) remain_pot += 1;
+            else remain_pot += s.data.pot * pen;
+          }
+
+          let first_loop = true;
+          while (stat_positives.length) {
+            let s = stat_positives.shift();
+            let pen = this.determinePenalty([s, ...this.stats]);
+            if (Math.floor(s.data.pot * pen) >= this.potential) {
+              stat_positives.unshift(s);
+              break; // leave it in the positives to be statted and move on to the negs
+            }
+
+
+            remain_pot -= s.data.pot * pen; // remove from remain_pot
+            let pot_per_step = Math.floor(pen * s.data.pot);
+
+            // if there's one stat left after and it uses a penalty, dont leave any potential for it
+            if (stat_positives.length === 1) {
+              let s1 = stat_positives[0];
+              if (Math.floor(this.determinePenalty([s, s1, ...this.stats]) * s1.data.pot) !== s1.data.pot) {
+                // if it incurs a penalty anyways -
+                // stat as much of the current one as possible and treat the next one with the negs
+                remain_pot = 0;
+              }
+            }
+
+            let available_pot = this.potential - remain_pot;
+            let steps;
+
+            if (s.data.pot === 1) {
+              // these just need one point - there are no penalties for these even if done after the negs.
+              steps = 1
+            } else {
+              // max number of steps possible with available potential.
+              steps = Math.floor(available_pot / pot_per_step);
+            }
+            if (steps * pot_per_step >= this.potential + remain_pot) steps--; // always leave 1 point left
+            if (steps > s.value) steps = s.value;
+            if (first_loop) {
+              // the first one always is done in one chunk, as it will never be affected by potential.
+              this.applyStat([s.id, s.data, steps]);
+            } else {
+              // the ones after will be done 1 step at a time for a total number of steps.
+              for (let i = 0; i < steps; i++) this.applyStat([s.id, s.data, 1, true]);
+            }
+
+            first_loop = false;
+          }
+        }
+        // check if one neg has to be left out
+        if ((stat_positives.length ? 5 : 6) - this.stats.filter(s => s).length === negs.length) {
+          negs = negs.slice(0, -1);
+        }
+        // now deal with the negatives
+        if (stat_positives.length && negs.length) {
+          let s = stat_positives.shift();
+          this.applyStat([s.id, s.data, 1, true], ...negs.map(this.statToArray));
+        } else if (negs.length) {
+          // apply negs
+          this.applyStat(...negs.map(this.statToArray));
+        }
+
+        // now identify whichever ones are one pot - these are put in one at a time to avoid any penalties
+        let onepot = this.final_stats.filter(s => s.data.pot === 1);
+        for (let s of onepot) {
+            while ((this.stats[s.id] ? this.stats[s.id].value : 0) < s.value) {
+                this.applyStat([s.id, s.data, 1, true]);
+            }
+        }
+
+        // finally fill everything
+        this.applyStat(...this.final_stats.map(this.statToArray));
+
+        this.determineSuccess();
+    }
 
     determineSuccess() {
         let prev_pot = this.steps[this.steps.length - 2] ? this.steps[this.steps.length - 2][1] : this.original_potential;
@@ -471,6 +481,7 @@ function build_stat_dropdown() {
 function build_menu(initialize) {
   build_stat_dropdown();
   // build common stat formulas part - to be done later
+  show_formulas();
 }
 
 function get_results() {
@@ -489,7 +500,7 @@ function show_details(pot) {
   if (!Simulator.results[pot]) return;
   let formula = Simulator.results[pot];
   let buffer = `<table style="width:100%"><tr><td style="width: 15%; color: green; font-weight: bold; text-align: left">POT ${pot}</td><td style="width: 70%"></td><td style="width:15%; color: green; font-weight: bold; text-align: right">${formula.success.toFixed(2)}%</td></tr>`;
-  buffer += `<tr><th>Step</th><th>Change</th><th>Pot</th></tr>`;
+  buffer += `<tr><th style="text-align: left">Step</th><th style="text-align: left">Change</th><th  style="text-align: right">Pot</th></tr>`;
 
   let s = 0;
   for (let step of formula.steps) {
@@ -499,10 +510,50 @@ function show_details(pot) {
   buffer += '</table><br /><h3 style="text-align: center">Materials Used</h3><table style="width:100%"><tr><th style="width:75%">Materials</th><th>Pts.</th></tr>'
 
   for (let m in formula.mats) {
-    buffer += `<tr><td>${m}</td><td>${formula.mats[m]}</td></tr>`;
+    buffer += `<tr><td style="text-align: left">${m}</td><td style="text-align: right">${formula.mats[m]}</td></tr>`;
   }
 
   buffer += '</table>';
 
   document.getElementById('details').innerHTML = buffer;
+}
+
+function escapeHTML (str) {
+    return ('' + str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&apos;').replace(/\//g, '&#x2f;');
+}
+
+function show_formulas() {
+  let buffer = Formulas.sort((a,b) => b.weap_arm.localeCompare(a.weap_arm)).map(f => {
+    let json = escapeHTML(JSON.stringify(f));
+    return `<button class="formula_button" onclick='set_formula(${json})' style="width: 100%; text-align: left">${f.weap_arm === 'w' ? '<span style="color: red; font-weight: bold">Weapon</span>' : '<span style="color: Green; font-weight: bold">Armor</span>'}<br />${f.stats.filter(s => s[1] > 0 || s[2]).map(s => FORMULA.toStepString(...s)).join('<br />')}</button>`
+  }).join('<br />');
+  document.getElementById('stat_formulas').innerHTML = buffer;
+}
+
+function set_formula(data) {
+  Simulator.weap_arm = data.weap_arm;
+  let i = 0;
+  for (let s of data.stats) Simulator.setStat(i++, ...s);
+  sync_ui_with_sim();
+  get_results();
+}
+
+function reverse_options_to_index(name) {
+  for (let i = 0; i < OPTIONS.length; i++) {
+    if (OPTIONS[i].name === name) return i + 1;
+  }
+  return 0;
+}
+
+function sync_ui_with_sim () {
+  document.getElementById('weap_arm').value = Simulator.weap_arm;
+  build_stat_dropdown();
+
+  for (let i = 0; i < Simulator.stats.length; i++) {
+    let s = Simulator.stats[i];
+    if (!s) continue;
+
+    document.getElementById('slot' + i).value = reverse_options_to_index(s.name);
+    document.getElementById('slot' + i + '_value').value = s.value;
+  }
 }
