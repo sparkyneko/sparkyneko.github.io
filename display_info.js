0d141ff68f61122f726835948e32e13c741d86f6
Charlie Kobayashi <sparkychildcharlie@gmail.com>
2019-03-19 12:11:31 -0400

    Create display_info.js

---
+++ display_info.js
@@ -0,0 +1,106 @@
+var slides = [
+    // 1
+    `!<h3>Daniel Thompson: Aesthetics, Authenticities, and Appeals to Authority</h3><br /><br /><center>Pleasing the audience</center>`,
+
+    // 2
+    `What is Ethnomusicology?
+    - "study of music from the cultural and social aspects of the people who make it"
+    - the author followed some debates by ethnomusicologists`,
+
+    `"Authenticity"?
+    - understanding of music involving historical performance practices
+    - "Authenticity is knowing what you are, and acting in accordance with that knowledge" - Taruskin
+
+    Authentic to: 
+    - composer's intentions
+    - performance practice of that era
+    - how music and instruments sounded like during the composer's era
+    - "personal authenticity" - faithful to the performer`,
+
+    // 3
+    `The Listeners
+    - whether or not listeners are under peer pressure to like or dislike a certain type of music.
+
+    A listener should reflect
+    - whether they like the performance
+    - whether the performance means anything to them`,
+
+    // 4
+    `
+    - Musical meaning can be located in the listener
+    - John Dewey says that a work of art is a product (the music) combined with a human being (the listener), in which the listener enjoys the experience.
+    - Should performers serve the tastes of the audience?
+    - Was discussed in a music journal in Europe, 1997`,
+
+    // 5
+    `
+    - Listeners tend to want to be in the majority regarding whether or not they liked or disliked a concert.
+    - Listeners categorize what they call a good or bad performance, but don't understand what is good or bad.
+    - Good as in a "well informed performance" or "aesthetically pleasing performance"
+    - Dislike or like the music, composer, genre instead of the performance?
+    - Whether or not all listeners should react the same way to a concert
+    - Listeners should say whether they like or dislike music, instead of good vs bad performance
+    - #mus[0] vs. #mus[1]`,
+
+    // 6
+    `Performer's Goals
+    - What performer wants to convey?
+    - What the music would have sounded like during the composer's lifetime
+    - What the composer wished for the listener to hear
+    - May have to break historical performance practice to obtain what the composer desired.`,
+
+    // 7
+    `Conveying to the Listener
+    - How listeners interpret music
+    - Performers need to communicate the composers message in a manner that is understood by the listeners
+    - (argument used by those in support of Western art music tradition)`,
+
+    // 8
+    `Listener's view on Authenticity
+    - listeners are able to listen to music that matches their tastes
+    - many listeners don't care as much about the historical accuracy, but rather just enjoy the sound of the instrument. #mus[2] vs. #mus[3]
+    - listeners judge music comparing to whatever they hear the most.
+    - a "aesthetically good" performances to the listener uses the practice the listener is most familiar with.
+    - using the instruments that the listener likes to hear.
+    - Increase in people liking early music instruments is because of the increased availability of performances on such instruments`,
+
+    // 9
+    `
+    - performance practice always existed, to please the living composers as well as the listener.
+    - Interpretation: performer's role, audience's role.
+    - only electronic/computer music is free of interpretation`,
+
+    // 10
+    `Authenticity of performance venue
+    - Affects how the listener is able to perceive the music
+    - some music is designed to be listened to with ambient noise
+    - the acoustics of the hall - size and material
+    - balance of the performance
+
+    Sound recordings, with mixing to recreate the ambience of the performance space, "the sound technician has become a main participant in the interpretation of early music" (Goldberg)`,
+
+    // 11
+    `Authority
+    - The "authentic" way of playing would have authority on how music is to be played
+    - competed through aesthetics of the listener
+    - Tomilson believes that what the composer originally intended, and what the original audience heard is very different compared to what performers believe the composer's original meaning`,
+
+    // 12
+    `Aesthetics of authentic performances
+    - May simply be a fresh view on music that the listeners want
+    - Has nothing to do with whether or not it's authentic
+    - No authenticity in fight on opinions of "aesthetics", only research has any hard facts.
+    - Performer must understand what the listener's aesthetic preferences are, and to let their music reach them in a performance.`
+];
+
+var music = [
+    // 1
+    {name: "Gould", url: './files/Gould.mp3'},
+    
+    // 2
+    {name: "Richter", url: './files/Richter.mp3'},
+
+    // 3
+    {name: 'Ich Ruf zur dir: Bach-Busoni transcription', url: './files/Piano.mp3'},
+    {name: 'Ich Ruf zur dir: original organ', url: './files/Organ.mp3'},
+]
