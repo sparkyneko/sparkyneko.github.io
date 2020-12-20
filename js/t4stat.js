246811dda5b175c822bbe343f2ee507992be3c70
Charlie <sparkychildcharlie@gmail.com>
2020-12-20 09:26:49 -0500

    Finalize pot return value

--- js/t4stat.js
+++ js/t4stat.js
@@ -581,7 +581,7 @@ class Stat {
         this.step_max_mats = 0;
 
         this.potential_return = 5 + details.tec / 10;
-        this.bonus_potential_return = 7.65 /* toram_round(3 + details.tec / 51) */;
+        this.bonus_potential_return = this.potential_return / 4;
 
         this.finished = false;
 
